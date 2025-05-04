9f26e623c0e69f17db63b96a8c207d0daf904b30
sh1dan <76394617+sh1dan@users.noreply.github.com>
2025-05-04 18:36:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({ ok: true });
-}
-
 // ====== Canvas & Context ======
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
